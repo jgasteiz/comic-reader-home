4f4e0d01f3488323b85afa2cd61ff1df36d2dfad
Javi Manzano <javi.manzano.oller@gmail.com>
2017-12-21 22:36:40 +0000

    Add hacky keyboard navigation with jquery

---
+++ static/js/reader.js
@@ -0,0 +1,24 @@
+(function () {
+
+    function goToNextPage() {
+        if (reader.nextPageUrl) {
+            window.location = reader.nextPageUrl;
+        }
+    }
+
+    function goToPreviousPage() {
+        if (reader.previousPageUrl) {
+            window.location = reader.previousPageUrl;
+        }
+    }
+
+    $(document).keydown(function(e) {
+        // left, up: previous page
+        if (e.which === 37 || e.which === 38) {
+            goToPreviousPage();
+        }
+        else if (e.which === 39 || e.which === 40) {
+            goToNextPage();
+        }
+    });
+})();
