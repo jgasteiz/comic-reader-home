9a5464cfce3ea624ba69a6e547f82c4da8369d2e
Javi Manzano <javi.manzano.oller@gmail.com>
2017-12-23 12:23:22 +0000

    Handle comic page and nex/previous page urls fetching in React to improve page load

--- static/react/reader.js
+++ static/react/reader.js
@@ -1,16 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const READER = document.getElementById('reader');
+
+
+class Reader extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pageSrc: '',
+            previousPageUrl: '',
+            nextPageUrl: '',
+        }
+    }
+
+    render() {
+        return (
+            <div className={"reader reader--" + (this.state.pageSrc ? 'show' : 'loading')}>
+                <Navigation
+                    nextPageUrl={this.state.nextPageUrl}
+                    previousPageUrl={this.state.previousPageUrl}
+                />
+                <ComicPage
+                    pageSrc={this.state.pageSrc}
+                />
+                <BatmanSpinner/>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        this.getImageSrc();
+    }
+
+    getImageSrc() {
+        const component = this;
+
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', `/api/${READER.dataset.comicPath}/${READER.dataset.comicPage}/`);
+        httpRequest.setRequestHeader('Content-Type', 'application/json');
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    component.setState({
+                        pageSrc: JSON.parse(this.response)['page_src'],
+                        nextPageUrl: JSON.parse(this.response)['next_page'],
+                        previousPageUrl: JSON.parse(this.response)['previous_page'],
+                    });
+                } else {
+                    alert('There was a problem with the request.');
+                }
+            }
+        };
+        httpRequest.send();
+    }
+}
+
+
+class Navigation extends React.Component {
+    render() {
+        return (
+            <nav className="page-navigation">
+                <a className="btn btn-light"
+                   href={this.props.previousPageUrl}
+                >Previous page</a>
+                <a className="btn btn-light"
+                   href={this.props.nextPageUrl}
+                >Next page</a>
+            </nav>
+        );
+    }
+}
+
+
+class BatmanSpinner extends React.Component {
+    render() {
+        return (
+            <div className="batman-spinner">
+                <img src="/static/img/batman.png"/>
+            </div>
+        )
+    }
+}
+
+
 class ComicPage extends React.Component {
     render() {
-        const pageSrc = document.getElementById('page').attributes.getNamedItem('data-src').value;
         return (
-            <img className="comic-page" src={pageSrc}/>
+            <img className="comic-page"
+                 src={this.props.pageSrc}
+            />
         );
     }
 }
 
 ReactDOM.render(
-    <ComicPage/>,
-    document.getElementById('page')
+    <Reader/>,
+    document.getElementById('reader')
 );
