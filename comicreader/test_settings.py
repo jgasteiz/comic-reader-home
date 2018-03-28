fec9ea1f6801960237104b33579cb3dc2c56b1ec
Javi Manzano <javi.manzano.oller@gmail.com>
2018-03-28 16:51:52 +0100

    Fix tests

--- comicreader/test_settings.py
+++ comicreader/test_settings.py
@@ -2,6 +2,7 @@ from .settings import *
 
 SECRET_KEY = '123456'
 
+COMIC_EXTRACT_PATH = os.path.join(BASE_DIR, 'static/comics/tmp')
 COMICS_ROOT = os.path.join(BASE_DIR, 'comicreader/tests_fixtures/comics')
 STATIC_ROOT = ''
 USE_CELERY = False
