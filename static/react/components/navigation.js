01623a820dc688560ab7887e74346388aa0d3e58
Javi Manzano <javi.manzano.oller@gmail.com>
2018-04-13 10:40:09 +0100

    Remove titles from navigation buttons
    Less accessible, but also less annoying. I don’t want the browser tooltips appearing on mouse hover

--- static/react/components/navigation.js
+++ static/react/components/navigation.js
@@ -10,11 +10,9 @@ export default class Navigation extends React.Component {
             <nav className="page-navigation">
                 <a className={"page-navigation-item page-navigation-item--previous page-navigation-item--" + (this.props.hasPreviousPage ? 'show' : 'hidden')}
                    onClick={this.props.previousPageHandler}
-                   title="Previous page"
                 />
                 <a className={"page-navigation-item page-navigation-item--next page-navigation-item--" + (this.props.hasNextPage ? 'show' : 'hidden')}
                    onClick={this.props.nextPageHandler}
-                   title="Next page"
                 />
             </nav>
         );
