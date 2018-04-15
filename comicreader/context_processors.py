a902cf641128465a0c4497dda45e5f5d93d73860
Javi Manzano <javi.manzano.oller@gmail.com>
2018-04-15 18:09:41 +0100

    Add a context processor for rendering the revision at the end of the static file names

---
+++ comicreader/context_processors.py
@@ -0,0 +1,48 @@
+"""
+All this has been taken from https://github.com/dobarkod/django-source-revision.
+"""
+
+from subprocess import CalledProcessError
+from django.conf import settings
+
+try:
+    from subprocess import check_output
+except ImportError:
+    from subprocess import Popen, PIPE
+    def check_output(cmd):
+        return Popen(cmd, stdout=PIPE).communicate()[0]
+
+__all__ = ['get_revision']
+
+_revision_loaded = False
+_revision = None
+
+
+def get_revision():
+    global _revision, _revision_loaded
+
+    if _revision_loaded:
+        return _revision
+
+    commands = getattr(settings, 'SOURCE_REVISION_COMMANDS', [
+        'git rev-parse --short HEAD',
+        'hg id -i'
+    ])
+
+    def get_rev():
+        for command in commands:
+            try:
+                rev = check_output(command.split(u' ')).decode('ascii').strip()
+            except (CalledProcessError, OSError):
+                continue
+            if rev:
+                return rev
+        return None
+
+    _revision = get_rev()
+    _revision_loaded = True
+    return _revision
+
+
+def source_revision(request):
+    return {u'SOURCE_REVISION': get_revision()}
