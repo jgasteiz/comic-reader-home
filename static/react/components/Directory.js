aedcdbaf7b011c9808df26099cf88165c80293c4
Javi Manzano <javi.manzano.oller@gmail.com>
2018-04-17 21:46:55 +0100

    Improve the Directory component rendering

--- static/react/components/Directory.js
+++ static/react/components/Directory.js
@@ -18,6 +18,73 @@ export default class Directory extends React.Component {
     }
 
     render() {
+        return (
+            <div>
+                {this._renderHeader()}
+
+                <section className="container">
+                    <header>
+                        <div className="row">
+                            <h1 className="col">
+                                {this.state.directoryName}
+                            </h1>
+                            <div className="col-auto">
+                                {this._renderBackLink()}
+                            </div>
+                        </div>
+                    </header>
+
+                    {this._renderComics()}
+                    {this._renderDirectories()}
+                </section>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        DirectoryService.getDirectoryDetails(this.props.match.params.dirPath, (res) => {
+            this.setState({
+                directories: res['path_contents']['directories'],
+                comics: res['path_contents']['comics'],
+                directoryPath: res['directory_path'],
+                parentPath: res['parent_path'],
+                directoryName: res['path_contents']['name'],
+            });
+        });
+    }
+
+    // Custom render functions
+
+    _renderHeader() {
+        return (
+            <header className="navbar navbar-dark bg-dark sticky-top navbar-expand-md">
+                <div className="container d-flex justify-content-between">
+                    <a href="/"
+                       className="navbar-brand d-flex align-items-center">
+                        <strong>Comic Reader</strong>
+                    </a>
+                </div>
+            </header>
+        );
+    }
+
+    _renderBackLink() {
+        const component = this;
+        return  (function() {
+            if (component.state.parentPath) {
+                return (
+                    <Link className="btn btn-secondary" to={`/dir/${component.state.parentPath}/`}>Back</Link>
+                );
+            } else {
+                return '';
+            }
+        })();
+    }
+
+    _renderComics() {
+        if (this.state.comics.length === 0) {
+            return '';
+        }
         const comicRows = this.state.comics.map(function (comic, index) {
             return (<tr key={comic.path}>
                 <td>
@@ -34,6 +101,30 @@ export default class Directory extends React.Component {
             </tr>);
         });
 
+        return (
+            <div>
+                <h2>Comics</h2>
+
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Comic name</th>
+                        <th scope="col" className="text-right">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {comicRows}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    _renderDirectories() {
+        if (this.state.directories.length === 0) {
+            return '';
+        }
         const directoryRows = this.state.directories.map(function (directory, index) {
             return (<tr key={directory.path}>
                 <td>
@@ -50,85 +141,24 @@ export default class Directory extends React.Component {
             </tr>);
         });
 
-        const component = this;
-        const backLink = (function() {
-            if (component.state.parentPath) {
-                return (
-                    <Link className="btn btn-secondary" to={`/dir/${component.state.parentPath}/`}>Back</Link>
-                );
-            } else {
-                return '';
-            }
-        })();
-
         return (
             <div>
-                <header
-                    className="navbar navbar-dark bg-dark sticky-top navbar-expand-md">
-                    <div className="container d-flex justify-content-between">
-                        <a href="/"
-                           className="navbar-brand d-flex align-items-center">
-                            <strong>Comic Reader</strong>
-                        </a>
-                    </div>
-                </header>
+                <h2>Directories</h2>
 
-                <section className="container">
-                    <header>
-                        <div className="row">
-                            <h1 className="col">
-                                {this.state.directoryName}
-                            </h1>
-                            <div className="col-auto">
-                                {backLink}
-                            </div>
-                        </div>
-                    </header>
-
-                    <h2>Comics</h2>
-
-                    <table className="table table-striped">
-                        <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Comic name</th>
-                            <th scope="col" className="text-right">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {comicRows}
-                        </tbody>
-                    </table>
-
-                    <h2>Directories</h2>
-
-                    <table className="table table-striped">
-                        <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Directory name</th>
-                            <th scope="col" className="text-right">Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                            {directoryRows}
-                        </tbody>
-                    </table>
-                </section>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Directory name</th>
+                        <th scope="col" className="text-right">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {directoryRows}
+                    </tbody>
+                </table>
             </div>
         );
-    }
-
-    componentDidMount() {
-        DirectoryService.getDirectoryDetails(this.props.match.params.dirPath, (res) => {
-            this.setState({
-                directories: res['path_contents']['directories'],
-                comics: res['path_contents']['comics'],
-                directoryPath: res['directory_path'],
-                parentPath: res['parent_path'],
-                directoryName: res['path_contents']['name'],
-            });
-        });
     }
 
 
