815109aa8ef270269330238ecadf098eb7e51baf
Javi Manzano <javi.manzano.oller@gmail.com>
2018-04-20 22:38:22 +0100

    Add a custom runserver command for also running webpack-watch

---
+++ reader/management/commands/yarnrunserver.py
@@ -0,0 +1,21 @@
+from subprocess import Popen, PIPE
+import logging
+from django.contrib.staticfiles.management.commands import runserver
+
+
+class Command(runserver.Command):
+    help = "Starts a lightweight Web server for development and also serves static files."
+
+    def run(self, *args, **options):
+        watch_proc = None
+        watch_proc = Popen(['yarn', 'run', 'webpack-watch'])
+        try:
+            logging.info('Starting localhost:')
+            super(Command, self).run(*args, **options)
+        except (BaseException, Exception) as e:
+            try:
+                if watch_proc:
+                    watch_proc.kill()
+            except OSError:
+                pass
+            raise e
