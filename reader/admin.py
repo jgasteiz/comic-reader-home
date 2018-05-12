2287c78c7ad9154156d6d192a84151b4616a2384
Javi Manzano <javi.manzano.oller@gmail.com>
2018-05-12 22:44:45 +0100

    New model and migration

--- reader/admin.py
+++ reader/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Bookmark, Favorite
+from .models import Bookmark, Favorite, FileItem
 
 admin.site.register(Bookmark)
 admin.site.register(Favorite)
+admin.site.register(FileItem)
--- reader/models.py
+++ reader/models.py
@@ -1,4 +1,44 @@
+import os
+
+from django.conf import settings
 from django.db import models
+
+
+class FileItem(models.Model):
+    COMIC = 'comic'
+    DIRECTORY = 'directory'
+    FILE_TYPE_CHOICES = (
+        (COMIC, COMIC),
+        (DIRECTORY, DIRECTORY),
+    )
+
+    name = models.CharField(max_length=512, blank=True)
+    path = models.TextField(blank=True)
+    file_type = models.CharField(max_length=12, choices=FILE_TYPE_CHOICES, blank=True)
+
+    parent = models.ForeignKey('FileItem', related_name='children', on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        ordering = ['-file_type', 'name']
+
+    def __str__(self):
+        return '%s - %s' % (self.file_type, self.name)
+
+    def save(self, *args, **kwargs):
+        # Set the directory name
+        self.name = self.path.split('/')[-1]
+        # Set the file type
+        self.file_type = self.DIRECTORY if os.path.isdir(self.path) else self.COMIC
+        super(FileItem, self).save(*args, **kwargs)
+
+    @property
+    def is_root(self):
+        return self.path == settings.COMICS_ROOT
+
+    @property
+    def encoded_path(self):
+        from reader.utils import get_encoded_path
+        return get_encoded_path(self.path)
 
 
 class Bookmark(models.Model):
