95e4b9c75033f4bd19634758f810e6e5dc53e569
Javi Manzano <javi.manzano.oller@gmail.com>
2018-05-12 17:39:08 +0100

    Split settings into different settings modules

--- comicreader/settings/common.py
+++ comicreader/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -127,3 +129,11 @@ IGNORED_FILE_NAMES = [
 ]
 
 PAGE_NOT_FOUND = '/static/img/404.jpg'
+
+# Reader settings
+DEFAULT_COMIC_EXTRACT_PATH = os.path.join(BASE_DIR, 'static/comics/tmp')
+COMIC_EXTRACT_PATH = os.environ.get('COMIC_EXTRACT_PATH', DEFAULT_COMIC_EXTRACT_PATH)
+if os.environ.get('USE_DOCKER') == 'true':
+    COMICS_ROOT = '/comics'
+else:
+    COMICS_ROOT = os.environ.get('COMICS_ROOT')
--- comicreader/settings/test.py
+++ comicreader/settings/test.py
@@ -1,7 +1,7 @@
-from .settings import *
+from .common import *
 
 SECRET_KEY = '123456'
+DEBUG = False
 
 COMIC_EXTRACT_PATH = os.path.join(BASE_DIR, 'static/comics/tmp')
 COMICS_ROOT = os.path.join(BASE_DIR, 'comicreader/tests_fixtures/comics')
-STATIC_ROOT = ''
