ff8a018469236fb6da78f9a07ee3f14ad54a0262
Javi Manzano <javi.manzano.oller@gmail.com>
2018-05-13 10:58:21 +0100

    Move public and api views to a views module

--- reader/urls.py
+++ reader/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from . import api
-from . import views
+from .views import old_api, public
 
 
 app_name = 'reader'
@@ -9,14 +8,15 @@ app_name = 'reader'
 
 urlpatterns = [
     # API
-    path('api/directory/', api.directory_view, name='api_directory_root'),
-    path('api/directory/<str:directory_path>/', api.directory_view, name='api_directory'),
-    path('api/comic/<str:comic_path>/', api.comic_detail, name='api_comic_detail'),
-    path('api/page/<str:comic_path>/<int:page_number>/', api.comic_page_src, name='api_comic_page_src'),
-    path('api/bookmark/', api.bookmark_comic_page, name='bookmark_comic_page'),
+    path('api/directory/', old_api.directory_view, name='api_directory_root'),
+    path('api/directory/<str:directory_path>/', old_api.directory_view, name='api_directory'),
+    path('api/comic/<str:comic_path>/', old_api.comic_detail, name='api_comic_detail'),
+    path('api/page/<str:comic_path>/<int:page_number>/', old_api.comic_page_src, name='api_comic_page_src'),
+    path('api/bookmark/', old_api.bookmark_comic_page, name='bookmark_comic_page'),
 
-    path('', views.comic_reader, name='comic_reader'),
-    path('dir/<str:directory_path>/', views.comic_reader, name='comic_reader'),
-    path('comic/<str:comic_path>/', views.comic_reader, name='comic_reader'),
-    path('comic/<str:comic_path>/<int:page_number>/', views.comic_reader, name='comic_reader'),
+    # Public views
+    path('', public.home, name='home'),
+    path('dir/<str:directory_path>/', public.home, name='home'),
+    path('comic/<str:comic_path>/', public.home, name='home'),
+    path('comic/<str:comic_path>/<int:page_number>/', public.home, name='home'),
 ]
--- reader/views/public.py
+++ reader/views/public.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def comic_reader(request, *args, **kwargs):
+def home(request, *args, **kwargs):
     return render(request, template_name='reader/index.html')
