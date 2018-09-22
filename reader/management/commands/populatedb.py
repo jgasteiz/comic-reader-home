8e95f2596925a0de02839ff4f9a1e2bf308a379c
Javi Manzano <javi.manzano.oller@gmail.com>
2018-09-22 20:32:33 +0100

    Fix bug in the populatedb management command
    We only want to delete old files once, not once per directory

--- reader/management/commands/populatedb.py
+++ reader/management/commands/populatedb.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand
-from reader.tasks import populate_db_from_path
+from reader import tasks
 
 
 class Command(BaseCommand):
     help = 'Populate the db with all comics and directories available.'
 
     def handle(self, *args, **kwargs):
-        populate_db_from_path()
+        tasks.delete_old_items()
+        tasks.populate_db_from_path()
         self.stdout.write(self.style.SUCCESS('The DB has been populated.'))
--- reader/tasks.py
+++ reader/tasks.py
@@ -6,18 +6,21 @@ from reader.models import FileItem
 from reader.utils import is_file_name_comic_file
 
 
-def populate_db_from_path(path=settings.COMICS_ROOT, parent=None):
-    file_item, _ = FileItem.objects.get_or_create(path=path, parent=parent)
-
-    # First check all existing directories and comics actually exist
-    # and delete the ones that don't.
+def delete_old_items():
+    """
+    Delete all FileItems which paths don't exist anymore.
+    """
     all_file_items = FileItem.objects.all()
     for file_item in all_file_items:
         if not os.path.exists(file_item.path):
             print('Deleting %s' % file_item.name)
             file_item.delete()
 
-    # Start creating FileItem recursively, starting from the COMICS_ROOT.
+
+def populate_db_from_path(path=settings.COMICS_ROOT, parent=None):
+    file_item, _ = FileItem.objects.get_or_create(path=path, parent=parent)
+
+    # Start creating FileItem recursively.
     for path_name in os.listdir(path):
         # Skip hidden files/directories.
         if path_name.startswith('.'):
