56e06dacc1ed4542fbaf3158936d36194055a343
Javi Manzano <javi.manzano.oller@gmail.com>
2020-08-03 07:49:45 +0100

    Remove all api-related code

--- reader/urls.py
+++ reader/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
-from rest_framework import routers
 
-from .views import api, public
-
-router = routers.DefaultRouter()
-router.register(r"fileitems", api.FileItemViewSet)
+from . import views
 
 app_name = "reader"
 
 
 urlpatterns = [
-    path("", public.directory, name="directory"),
-    path("dir/<int:fileitem_id>/", public.directory, name="directory"),
-    path("comic/<int:comic_id>/", public.read_comic_page, name="read_comic_page",),
+    path("", views.directory, name="directory"),
+    path("dir/<int:fileitem_id>/", views.directory, name="directory"),
+    path("comic/<int:comic_id>/", views.read_comic_page, name="read_comic_page",),
     path(
         "comic_page/<int:comic_id>/<int:page_number>/",
-        public.comic_page_src,
+        views.comic_page_src,
         name="comic_page_src",
     ),
 ]
