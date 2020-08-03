7e00527193eaa1a9ff16a5ffd8dda84e3dcd1c65
Javi Manzano <javi.manzano.oller@gmail.com>
2020-08-03 22:13:34 +0100

    Add tests

---
+++ tests/settings.py
@@ -0,0 +1,12 @@
+from core.settings import *
+
+SECRET_KEY = "123456"
+DEBUG = False
+
+COMIC_EXTRACT_PATH = os.path.join(BASE_DIR, "static/comics/tmp")
+COMICS_ROOT = os.path.join(BASE_DIR, "tests/fixtures/comics")
+
+DATABASES["default"] = {
+    "ENGINE": "django.db.backends.sqlite3",
+    "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+}
---
+++ tests/test_views.py
@@ -0,0 +1,72 @@
+import pytest
+from django import shortcuts
+from django.test import Client
+
+from reader import models, tasks
+
+pytestmark = pytest.mark.django_db
+
+
+def test_directory_root():
+    tasks.populate_db_from_path()
+    client = Client()
+    root_directory = models.FileItem.objects.get(
+        file_type=models.FileItem.DIRECTORY, parent__isnull=True
+    )
+    comic_directory = models.FileItem.objects.get(
+        file_type=models.FileItem.DIRECTORY, parent=root_directory
+    )
+
+    url = shortcuts.reverse("reader:directory")
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.context["parent"] == root_directory
+    assert response.context["directory_list"].count() == 1
+    assert response.context["directory_list"].first() == comic_directory
+    assert response.context["comic_list"].count() == 0
+
+
+def test_directory_detail():
+    tasks.populate_db_from_path()
+    client = Client()
+    comic_directory = models.FileItem.objects.get(
+        file_type=models.FileItem.DIRECTORY, parent__isnull=False
+    )
+    comic = models.FileItem.objects.get(
+        file_type=models.FileItem.COMIC, parent=comic_directory
+    )
+
+    url = shortcuts.reverse(
+        "reader:directory", kwargs={"fileitem_id": comic_directory.id}
+    )
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.context["parent"] == comic_directory
+    assert response.context["directory_list"].count() == 0
+    assert response.context["comic_list"].count() == 1
+    assert response.context["comic_list"].first() == comic
+
+
+def test_comic_detail():
+    tasks.populate_db_from_path()
+    client = Client()
+    comic_directory = models.FileItem.objects.get(
+        file_type=models.FileItem.DIRECTORY, parent__isnull=False
+    )
+    comic = models.FileItem.objects.get(
+        file_type=models.FileItem.COMIC, parent=comic_directory
+    )
+
+    url = shortcuts.reverse("reader:read_comic_page", kwargs={"comic_id": comic.id})
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert response.context["comic_id"] == comic.id
+    assert response.context["previous_page_number"] == -1
+    assert response.context["next_page_number"] == 1
+    assert response.context["parent_id"] == comic_directory.id
+    assert response.context["num_pages"] == 3
+    assert response.context["current_page_number"] == 0
+    assert response.context["current_page_width"] == 100
