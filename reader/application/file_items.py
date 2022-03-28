9305d76a63d6b7e3bb2c0600689111d1d9e513a0
Javi Manzano <javi.manzano.oller@gmail.com>
2022-03-28 20:13:31 +0100

    Move tasks to application layer

--- reader/application/file_items.py
+++ reader/application/file_items.py
@@ -1,7 +1,6 @@
 import os
 
 from django.conf import settings
-
 from reader import domain, models
 
 
--- reader/management/commands/populatedb.py
+++ reader/management/commands/populatedb.py
@@ -1,12 +1,11 @@
 from django.core.management.base import BaseCommand
-
-from reader import tasks
+from reader.application import file_items
 
 
 class Command(BaseCommand):
     help = "Populate the db with all comics and directories available."
 
     def handle(self, *args, **kwargs):
-        tasks.delete_old_items()
-        tasks.populate_db_from_path()
+        file_items.delete_old_items()
+        file_items.populate_db_from_path()
         self.stdout.write(self.style.SUCCESS("The DB has been populated."))
